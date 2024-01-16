583c7d38fd0381e691ea69cd0ff71b53a6f72515
Ishan Malviya <ishan.malviya@gmail.com>
2024-01-16 17:24:02 +0530

    Gets the certificate from static url using rustls

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,83 @@
 mod config;
 
 use openssl::ssl::{SslConnector, SslMethod};
-use std::net::TcpStream;
+use rustls::{ClientConfig, RootCertStore};
+use std::{net::TcpStream, sync::Arc};
+
+
+use std::io::{stdout, Read, Write};
+
 
 use clap::Parser;
 
 pub fn run() {
-    get_args();
+    //get_args();
+    new_cert_data();
 }
 
 fn get_args() {
     let config = config::config::Config::parse();
     get_certificate_data_for_domain(&config.domain);
+    new_cert_data();
+}
+
+fn new_cert_data() {
+    let mut root_store = RootCertStore::empty();
+    root_store.extend(
+        webpki_roots::TLS_SERVER_ROOTS
+            .iter()
+            .cloned(),
+    );
+
+    // Allow using SSLKEYLOGFILE.
+    let mut config = rustls::ClientConfig::builder()
+        .with_root_certificates(root_store)
+        .with_no_client_auth();
+
+    // Allow using SSLKEYLOGFILE.
+    config.key_log = Arc::new(rustls::KeyLogFile::new());
+
+    let server_name = "www.google.com".try_into().unwrap();
+    let mut conn = rustls::ClientConnection::new(Arc::new(config), server_name).unwrap();
+    let mut sock = TcpStream::connect("www.google.com:443").unwrap();
+    let mut tls = rustls::Stream::new(&mut conn, &mut sock);
+   
+    tls.write_all(
+        concat!(
+            "GET / HTTP/1.1\r\n",
+            "Host: www.google.com\r\n",
+            "Connection: close\r\n",
+            "Accept-Encoding: identity\r\n",
+            "\r\n"
+        )
+        .as_bytes(),
+    )
+    .unwrap();
+    let ciphersuite = tls
+        .conn
+        .negotiated_cipher_suite()
+        .unwrap();
+    writeln!(
+        &mut std::io::stderr(),
+        "Current ciphersuite: {:?}",
+        ciphersuite.suite()
+    )
+    .unwrap();
+
+
+    let server_cert_chain = conn.peer_certificates().unwrap();
+    // Print information about each certificate in the chain
+    for (index, cert) in server_cert_chain.iter().enumerate() {
+        //let cert_pem = rustls::HandshakeType::Certificate
+        println!("{:?}", cert);
+        // println!("Certificate #{}:", index + 1);
+        // println!("Subject: {}", cert.subject());
+        // println!("Issuer: {}", cert.issuer());
+        // println!("Not Before: {}", cert.validity().not_before);
+        // println!("Not After: {}", cert.validity().not_after);
+        // println!("Serial Number: {}", cert.serial_number());
+        println!();
+    }
 }
 
 fn get_certificate_data_for_domain(domain: &str) {
